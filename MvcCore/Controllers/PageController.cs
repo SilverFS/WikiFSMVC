fa2e8216c01cecc6dffe4a0cfc03421a55d0ed76
SilverFS <f.susetyo@outlook.com>
2021-11-02 09:28:46 +0100

    Added edit page function

--- MvcCore/Controllers/PageController.cs
+++ MvcCore/Controllers/PageController.cs
@@ -50,6 +50,31 @@ namespace MvcCore.Controllers
             return allText;
         }
 
+        public Page GetPage(int ID)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT ID, Title, Text FROM TextTable WHERE ID=@ID", connection);
+                {
+                    command.Parameters.AddWithValue("ID", ID);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            return new Page
+                            {
+                                ID = Convert.ToInt32(reader["ID"].ToString()),
+                                Title = reader["Title"].ToString(),
+                                Text = reader["Text"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
         public void CreatePage(Page page)
         {
             using (SqlConnection connection = new SqlConnection(_connectionString))
@@ -93,6 +118,38 @@ namespace MvcCore.Controllers
         }
 
 
+
+        public void EditPage(Page page)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("UPDATE TextTable SET Title= @Title, Text = @Text WHERE ID=@ID", connection);
+                {
+                    if (page.Title == null)
+                    {
+                        command.Parameters.AddWithValue("Title", DBNull.Value);
+                    }
+                    else
+                    {
+                        command.Parameters.AddWithValue("Title", page.Title);
+                    }
+                    if (page.Text == null)
+                    {
+                        command.Parameters.AddWithValue("Text", DBNull.Value);
+                    }
+                    else
+                    {
+                        command.Parameters.AddWithValue("Text", page.Text);
+                    }
+                    command.Parameters.AddWithValue("ID", page.ID);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
         public IActionResult Index()
         {
             return View(GetallText());
@@ -113,6 +170,19 @@ namespace MvcCore.Controllers
         public IActionResult Delete(int ID)
         {
             DeletePage(ID);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int ID)
+        {
+            return View(GetPage(ID));
+        }
+        
+        public IActionResult Edit(Page page, int ID)
+        {
+            page.ID = ID;
+            EditPage(page);
             return RedirectToAction("Index");
         }
 
